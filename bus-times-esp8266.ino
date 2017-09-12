9cd9d70115a9bef169e15645bbcd84b3e4bfe234
williamyeny <williamyeny@gmail.com>
2017-09-11 21:18:01 -0400

    added json parse

--- bus-times-esp8266.ino
+++ bus-times-esp8266.ino
@@ -2,11 +2,15 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include "keys.h"
+#include "SSD1306.h"
 
 void setup()
 {
   Serial.begin(921600);
   Serial.println();
+
+//  time_t t = now();
+//  Serial.println(hour(t));
 
   WiFi.begin("DukeOpen");
 
@@ -23,9 +27,12 @@ void setup()
   Serial.println("Mac address: " + WiFi.macAddress());
 
   HTTPClient http;
+
+  String stop = "4146366";
+  String routeIds[] = {"4008330", "4007592", "4005486", "4008336"};
   
   String fingerprint = "21:1E:D8:65:8C:D0:76:58:45:DB:70:A3:01:F4:E1:1C:13:BE:9E:A3"; // fingerprint of website
-  String url = "https://transloc-api-1-2.p.mashape.com/arrival-estimates.json?agencies=176&routes=4008330%2C+4007592%2C+4005486%2C+4008336&stops=4146366";
+  String url = "https://transloc-api-1-2.p.mashape.com/arrival-estimates.json?agencies=176&routes=4008330+4007592+4008336+4008336&stops=" + stop;
   
   http.begin(url, fingerprint);
   http.addHeader("Accept", "application/json");
@@ -47,12 +54,18 @@ void setup()
       JsonObject& root = jsonBuffer.parseObject(payload);
 
       if (!root.success()) {
-        Serial.println("whoops");
+        Serial.println("Error: failed to parse JSON");
       }
-      
-      int data = root["expires_in"];
 
-      Serial.println(data);
+      if (sizeof(root["data"]) == 0) {
+        Serial.println("No bus data available/no buses running");
+      } else {
+        JsonArray& arrivals = root["data"][0]["arrivals"];
+        for (int i = 0; i < sizeof(arrivals); i++) {
+          String arrivalTime = arrivals[i]["arrival_at"];
+          Serial.println(arrivalTime);
+        }
+      }
     }
   } else {
     Serial.println("[HTTP] GET... failed, error: " + http.errorToString(httpCode));
